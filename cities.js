cb61adb65d510e4a50b81164f945e457d1627f98
Daniel <141731225+danielsadegi@users.noreply.github.com>
2023-09-10 00:14:03 +0400

    Web-site

---
+++ cities.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Получаем параметры URL
+    const urlParams = new URLSearchParams(window.location.search);
+    
+    // Получаем значение параметра "city"
+    const selectedCity = urlParams.get('city');
+    
+    // Получаем ссылку на элемент <select> по его id
+    const citySelect = document.getElementById('citySelect');
+    
+    // Устанавливаем выбранный город в выпадающем списке
+    if (selectedCity) {
+      citySelect.value = selectedCity;
+    }
+  });
